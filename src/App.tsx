59d02d4271e9de955d08aec39e27096c31ac95cb
해준 <127281057+ftery0@users.noreply.github.com>
2023-12-30 19:28:38 +0100

    feat :: 라우터 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "src/components/Main/Main/Main";
-
+import Profile from "./components/Profile/Profile/profile";
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element /> {/* 여기는 로그인 */}
+          <Route path="/main" element={<Main />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
