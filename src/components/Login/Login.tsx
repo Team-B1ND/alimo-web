4adbef91a8673ddf50a5c33b1c427dce2f479f2f
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-01-01 01:25:44 +0900

    ♻ ::	코드 리펙토링
    view 대충 완성

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -6,7 +6,6 @@ import IdCancel from "src/img/Id-Cancel.png";
 import PasswordHide from "src/img/Password-Hide.png";
 import PasswordShow from "src/img/Password-Show.png";
 import "src/style/Login.style/Login.css";
-import AlimoImage from "src/img/Alimo-image.png";
 
 const Login = () => {
   const [isInputClicked, setIsInputClicked] = useState(false);
