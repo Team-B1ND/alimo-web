ba7a2125d5ef261f86c57480829ec4ab79496423
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-01-16 16:23:14 +0900

    ⚙️ :: setting

--- src/components/WriteRead/WriteRead.tsx
+++ src/components/WriteRead/WriteRead.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import SideBar from "src/components/SideBar/SideBar";
+import * as S from "src/style/WriteRead.style/WriteRead.style";
 
 const MyWriteView = () => {
-    return (
-        <div>
-            <div></div>
-        </div>
-    )
-}
+  return (
+    <S.WriteRead>
+      <SideBar />
+      
+    </S.WriteRead>
+  );
+};
 
 export default MyWriteView;
