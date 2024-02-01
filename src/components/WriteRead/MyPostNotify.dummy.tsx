5664bbf00da3b4932b6630e0229f01de02ef76ac
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-02-01 09:26:57 +0900

    ♻ :: DummyContents 객체 타입 지정

--- src/components/WriteRead/MyPostNotify.dummy.tsx
+++ src/components/WriteRead/MyPostNotify.dummy.tsx
@@ -5,7 +5,11 @@ import { useNavigate } from "react-router-dom";
 
 const MyPostNotifyDummy = () => {
   const navigate = useNavigate();
-  const DUMMY_CONTENT = {
+  interface Dummy {
+    title: string;
+    contents: string;
+  }
+  const DUMMY_CONTENT: Dummy = {
     title: `<2023년 대한민국 고졸인재 채용엑스포 V-log 공모전 안내>`,
     contents: `위 공모전에 관심있는 학생은 이진주 선생님에게 카카오워크로 연락주세요~!`,
   };
