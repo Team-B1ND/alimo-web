ed7ad016cb662193c3baa7b48cecd564f7ddcd69
jeonminchan <chan2bo2@dgsw.hs.kr>
2024-02-06 18:39:35 +0900

    refactor :: Write #89

--- src/Hooks/Write/useWrite.ts
+++ src/Hooks/Write/useWrite.ts
@@ -1,67 +1,25 @@
-import React, { ChangeEvent, useEffect, useRef, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { showToast } from "src/lib/Toast/Swal";
-import CONFIG from "src/config.json";
-import axios from "axios";
+import React, { ChangeEvent, useRef, useState } from "react";
+
+interface Student {
+  name: string;
+}
+
 const useWrite = () => {
   const [title, setTitle] = useState<string>("");
-  const [content, setContent] = useState<string>("");
-  const [image, setImage] = useState<string | null>(null);
-  const [fileName, setFileName] = useState<string>("");
   const [file, setFile] = useState<File>();
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
-  const [contentAllow, setContentAllow] = useState<boolean>(false);
-  const [categoryAllow, setCategoryAllow] = useState<boolean>(false);
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const naviagate = useNavigate();
+  const [selectedCategory, setSelectedCategory] = useState<Student[]>([]);
+  const [fileName, setFileName] = useState<string>("");
+
+  const imageInputRef = useRef<HTMLInputElement>(null);
+  const FileInputRef = useRef<HTMLInputElement>(null);
 
   const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
-  }
-
-  const onChangeContent = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setContent(e.target.value);
   };
 
-  useEffect(() => {
-    if (content.length > 0) {
-      setContentAllow(true);
-    } else {
-      setContentAllow(false);
-    }
-  }, [content]);
-
-  const onChangeImageInput = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      const selectedFile = e.target.files[0];
-      setImage(URL.createObjectURL(selectedFile));
-    }
+  const handleImageClick = () => {
+    imageInputRef.current?.click();
   };
-
-  const handleClickButton = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
-  };
-
-  const handleCancelImage = () => {
-    setImage(null);
-    if (fileInputRef.current) {
-      fileInputRef.current.value = "";
-    }
-  };
-
-  const onClickCategory = (category: string) => {
-    setSelectedCategory((prevCategory) => (prevCategory === category ? null : category));
-  };
-
-  useEffect(() => {
-    if (selectedCategory === null) {
-      setCategoryAllow(false);
-    } else {
-      setCategoryAllow(true);
-    }
-  }, [selectedCategory]);
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files?.[0];
@@ -75,39 +33,27 @@ const useWrite = () => {
     setFile(selectedFile);
   };
 
-  const onClickConfirmButton = () => {
-    if (contentAllow && categoryAllow) {
-      showToast("success", "게시되었습니다.");
-      naviagate("/main");
-      axios.post(`${CONFIG.serverUrl}/notification/generate`, {
-        data: {
-          content: `${content}`,
-          role: [`${selectedCategory}`],
-        },
-        image: `${image}`,
-      });
-    } else if (!contentAllow && categoryAllow) {
-      showToast("error", "내용을 입력해주세요.");
+  const onClickAddCategory = (studentName: string) => {
+    const isSelected = selectedCategory.some((student) => student.name === studentName);
+
+    if (isSelected) {
+      setSelectedCategory(selectedCategory.filter((student) => student.name !== studentName));
     } else {
-      showToast("error", "카테고리를 선택해주세요.");
+      const newStudent: Student = { name: studentName };
+      setSelectedCategory([...selectedCategory, newStudent]);
     }
   };
 
   return {
     title,
-    content,
-    image,
+    onChangeTitle,
+    imageInputRef,
+    FileInputRef,
+    handleImageClick,
+    handleFileChange,
     fileName,
     selectedCategory,
-    fileInputRef,
-    onChangeTitle,
-    onChangeContent,
-    onChangeImageInput,
-    handleClickButton,
-    handleCancelImage,
-    onClickCategory,
-    handleFileChange,
-    onClickConfirmButton,
+    onClickAddCategory,
   };
 };
 
--- src/components/Write/Write.tsx
+++ src/components/Write/Write.tsx
@@ -2,23 +2,75 @@ import React from "react";
 import * as S from "src/style/Write.style/Write.style";
 import SideBar from "../SideBar/SideBar";
 import Header from "../SideBar/Header";
+import useWrite from "src/Hooks/Write/useWrite";
 import ImageUploadImg from "src/img/ImageUpload.svg";
 import FileUplaod from "src/img/FileUpload.svg";
 
 const Write = () => {
+  const { title, onChangeTitle, imageInputRef, handleFileChange, fileName, selectedCategory, onClickAddCategory } =
+    useWrite();
+
   return (
     <S.WriteMain>
       <SideBar />
       <Header />
       <S.WriteView>
         <S.InputWrap>
-          <S.WriteTitleInput placeholder="제목을 입력해주세요" />
+          <S.WriteTitleInput placeholder="제목을 입력해주세요" value={title} onChange={onChangeTitle} />
           <S.WriteContext placeholder="대소고에 새로운 소식을 전해보세요!" />
           <S.FileWrap>
-            <img src={ImageUploadImg} />
-            <img src={FileUplaod} />
+            <S.ImageUploadImg src={ImageUploadImg} />
+            <S.ImageInputRef type="file" accept="image/*" ref={imageInputRef} />
+            <S.FileChangeLabel htmlFor="file-change">
+              <img src={FileUplaod} />
+            </S.FileChangeLabel>
+            <S.ViewFileName value={fileName} readOnly />
+            <S.FileChange type="file" id="file-change" onChange={handleFileChange} />
           </S.FileWrap>
         </S.InputWrap>
+        <S.SelectCategoryWrap>
+          <S.SendCategoryWrap>
+            <S.SendCategoryTitle>보낼 카테고리를 선택해주세요.</S.SendCategoryTitle>
+            <S.CategoryWrap>
+              <S.Cateogory
+                onClick={() => onClickAddCategory("B1ND")}
+                isClicked={selectedCategory.some((student) => student.name === "B1ND")}
+              >
+                B1ND
+              </S.Cateogory>
+              <S.Cateogory
+                onClick={() => onClickAddCategory("B2ND")}
+                isClicked={selectedCategory.some((student) => student.name === "B2ND")}
+              >
+                B2ND
+              </S.Cateogory>
+              <S.Cateogory
+                onClick={() => onClickAddCategory("32ND")}
+                isClicked={selectedCategory.some((student) => student.name === "B3ND")}
+              >
+                B3ND
+              </S.Cateogory>
+              <S.Cateogory
+                onClick={() => onClickAddCategory("B4ND")}
+                isClicked={selectedCategory.some((student) => student.name === "B4ND")}
+              >
+                B4ND
+              </S.Cateogory>
+              <S.Cateogory
+                onClick={() => onClickAddCategory("B5ND")}
+                isClicked={selectedCategory.some((student) => student.name === "B5ND")}
+              >
+                B5ND
+              </S.Cateogory>
+            </S.CategoryWrap>
+          </S.SendCategoryWrap>
+          <S.UplaodButtonWrap>
+            <S.SendShowMember>
+              총 <span>120</span>명에게 전송돼요
+            </S.SendShowMember>
+            <S.UploadButton>게시하기</S.UploadButton>
+          </S.UplaodButtonWrap>
+        </S.SelectCategoryWrap>
       </S.WriteView>
     </S.WriteMain>
   );
