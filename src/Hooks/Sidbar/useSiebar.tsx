20e857a7d5f2303e883f8edb42df204e16e970a1
해준 <127281057+ftery0@users.noreply.github.com>
2024-02-20 03:24:53 +0900

    ⚡ ::	사이드바 훅 연결

---
+++ src/Hooks/Sidbar/useSiebar.tsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import useSideBarNavigation from "src/util/useSideBarNavigation";
+import CONFIG from "src/config/config.json"
+
+const Sidbar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [isProfileAlert, setProfileAlert] = useState(false);
+  const [isProfile, setProfile] = useState(false);
+  const [Name, setName] = useState("");
+  const [image, setimage] = useState("");
+    const ProfileInfo = async()=>{
+        try{
+            const response = await axios.get(`${CONFIG.serverUrl}/member/info`,{
+                headers: {
+                    Authorization : `Bearer ${""}`
+                }
+            });
+            const userData = response.data.data;
+            setName(userData.name)
+            setimage(userData.image)
+        }catch(error){
+            console.log(error);
+            
+        }
+    }
+  const OpenProfileSetting = () => {
+    setProfileAlert((prev) => !prev);
+  };
+  const openProfile = () => {
+    setProfile((prev) => !prev);
+  };
+  const { handleCategoryClick, isClickCategory } = useSideBarNavigation({
+    location,
+    navigate,
+  });
+  useEffect(()=>{
+    ProfileInfo();
+  },[]);
+  return {
+    Name,
+    image,
+    isProfileAlert,
+    isProfile,
+    OpenProfileSetting,
+    openProfile,
+    handleCategoryClick,
+    isClickCategory,
+  };
+};
+export default Sidbar;
