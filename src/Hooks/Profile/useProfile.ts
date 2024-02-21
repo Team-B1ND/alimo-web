0734faf7f63aa3e31cc6b77cf06db55105081a37
해준 <127281057+ftery0@users.noreply.github.com>
2024-02-22 04:03:07 +0900

    feat :: 프로필 이미지

--- src/Hooks/Profile/useProfile.ts
+++ src/Hooks/Profile/useProfile.ts
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import CONFIG from "src/config/config.json";
+import SidbarInfo from "src/Hooks/Sidbar/useSiebar";
+import ProfileImg from "src/assets/img/profileimg.png";
 
-const UseProfile =()=>{
-    const [selectedImg, setSelectedImg] = useState<string | null>(null);
+const UseProfile = () => {
+  const [selectedImg, setSelectedImg] = useState<string | null>(null);
+  const { Name, image } = SidbarInfo();
+  const finalImage = image.length > 0 ? image : ProfileImg;
+
   const handleChangeImg = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     const reader = new FileReader();
@@ -12,22 +18,25 @@ const UseProfile =()=>{
     if (file) {
       reader.readAsDataURL(file);
     }
-    try{
-        const response = await axios.patch("#",{
-            
-        })
-        if(response.status === 200){
-            console.log("success");   
-        }
 
-    }catch(error){
-        console.log(error);
+    try {
+      const response = await axios.patch(`${CONFIG.serverUrl}/`, {
         
+      });
+      if (response.status === 200) {
+        console.log("success");
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
-  return{
+
+  return {
+    Name,
+    image: finalImage,
     selectedImg,
     handleChangeImg,
-  }
-}
-export default UseProfile
+  };
+};
+
+export default UseProfile;
--- src/components/Profile/profile.tsx
+++ src/components/Profile/profile.tsx
@@ -1,12 +1,15 @@
 import * as S from "src/components/Profile/style/Profile.style";
 import SideBar from "src/constants/SideBar/SideBar";
-import ProfileImg from "src/assets/img/profileimg.png";
 import ChangeImg from "src/assets/img/Group 263.png";
+import ProfileImg from "src/assets/img/profileimg.png"
 import CloseImg from "src/assets/img/Closeimg.png"
 import UseProfile from "src/Hooks/Profile/useProfile";
 
+
 const Profile = ({ onClose }: { onClose: () => void }) =>{
   const {
+    Name,
+    image,
     selectedImg,
     handleChangeImg,
   } = UseProfile();
@@ -19,14 +22,14 @@ const Profile = ({ onClose }: { onClose: () => void }) =>{
           <S.ProfilePageNanme>프로필</S.ProfilePageNanme>
           <S.User>
             <S.profileImg>
-          <S.SetImg src={selectedImg || ProfileImg}></S.SetImg>
+          <S.SetImg><img src={selectedImg || image}/></S.SetImg>
             <S.ChangeFile>
               <label htmlFor="change-img">
                 <img src={ChangeImg} alt="Change Image" />
               </label>
               <S.Changbutton name="file" type="file" id="change-img" onChange={handleChangeImg} />
             </S.ChangeFile>
-            <span>이진주</span>
+            <span>{Name}</span>
             </S.profileImg>
           </S.User>
           <S.Category>
