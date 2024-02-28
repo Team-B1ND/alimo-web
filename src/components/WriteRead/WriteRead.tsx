8c7fe03a928a24874f5b63ee31403d6671b0fcf6
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-02-28 19:56:25 +0900

    rename :: MyPostNotify.dummy -> MyPostNotify로 파일 이름 변경 #94

--- src/components/WriteRead/WriteRead.tsx
+++ src/components/WriteRead/WriteRead.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SideBar from "src/constants/SideBar/SideBar";
-import MyPostNotifyDummy from "src/components/WriteRead/MyPostNotify.dummy";
+import MyPostNotifyDummy from "src/components/WriteRead/MyPostNotify";
 import * as S from "src/components/WriteRead/style/WriteRead.style";
+import CONFIG from "src/config/config.json";
+import axios from "axios";
 
 const MyWriteView = () => {
+  // const accesToken = localStorage.getItem("accestoken");
+  // useEffect(() => {
+  //   const NotificationLoad = async () => {
+  //     try {
+  //       await axios
+  //         .get(`${CONFIG.serverUrl}/notification/load`, {
+  //           params: { page: 0, size: 0 },
+  //           headers: {
+  //             Authorization: `Bearer ${accesToken}`,
+  //           },
+  //         })
+  //         .then((result) => {
+  //           console.log(result.data);
+  //         });
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   NotificationLoad();
+  // }, []);
+
   return (
     <S.WriteRead>
       <SideBar />
       <S.WriteReadView>
         <S.WriteReadWrap>
-          <MyPostNotifyDummy />
-          <MyPostNotifyDummy />
-          <MyPostNotifyDummy />
-          <MyPostNotifyDummy />
           <MyPostNotifyDummy />
         </S.WriteReadWrap>
       </S.WriteReadView>
