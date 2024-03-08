237f6f6f545534e8c50da36bb4e235685d654b90
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-03-08 15:19:32 +0900

    feat :: MyNotification 서버 통신 #94

--- src/components/MyNotification/MyNotification.tsx
+++ src/components/MyNotification/MyNotification.tsx
@@ -1,39 +1,51 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CONFIG from "src/config/config.json";
 import SideBar from "src/constants/SideBar/SideBar";
 import MyPostNotification from "src/components/MyNotification/MyPostNotification";
 import * as S from "src/components/MyNotification/style/MyNotification.style";
-import CONFIG from "src/config/config.json";
-import axios from "axios";
 
 const MyWriteView = () => {
-  // const accesToken = localStorage.getItem("accestoken");
-  // useEffect(() => {
-  //   const NotificationLoad = async () => {
-  //     try {
-  //       await axios
-  //         .get(`${CONFIG.serverUrl}/notification/load`, {
-  //           params: { page: 0, size: 0 },
-  //           headers: {
-  //             Authorization: `Bearer ${accesToken}`,
-  //           },
-  //         })
-  //         .then((result) => {
-  //           console.log(result.data);
-  //         });
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
+  const accessToken =
+  "eyJKV1QiOiJBQ0NFU1MiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiQXV0aG9yaXphdGlvbiI6IlRFQUNIRVIiLCJpYXQiOjE3MDk4Nzc4NjMsImV4cCI6MTcwOTg3OTY2M30.Fk3-EUFNVGB7l-762-dJGDpQtKIC_fRY4nyA5KmsOk4lajMIYUg6BNr9Fp6UKHQl-MHYNMROZG7t4uHrmmq-1Q";
+  interface NotificationType {
+    notificationId: number;
+  }
+  const [notificationData, setNotificationData] = useState<NotificationType[]>(
+    []
+  );
+  useEffect(() => {
+    const NotificationLoad = async () => {
+      try {
+        const response = await axios.get(
+          `${CONFIG.serverUrl}/notification/load/my`,
+          {
+            params: { page: 1, size: 100 },
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        setNotificationData(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-  //   NotificationLoad();
-  // }, []);
+    NotificationLoad();
+  }, []);
 
   return (
     <S.WriteRead>
       <SideBar />
       <S.WriteReadView>
         <S.WriteReadWrap>
-          <MyPostNotification />
+          {notificationData.map((data) => (
+            <MyPostNotification
+              key={data.notificationId}
+              notificationData={data}
+            />
+          ))}
         </S.WriteReadWrap>
       </S.WriteReadView>
     </S.WriteRead>
--- src/components/MyNotification/MyPostNotification.tsx
+++ src/components/MyNotification/MyPostNotification.tsx
@@ -1,42 +1,43 @@
-import MyProfile from "src/assets/img/Profile-Dummy.jpg";
-import MyNotifyImg from "src/assets/img/MyNotifyImg.svg";
-import * as S from "src/components/MyNotification/style/MyNotification.style";
 import { useNavigate } from "react-router-dom";
+import * as S from "src/components/MyNotification/style/MyNotification.style";
 
-const MyPostNotifyDummy = () => {
+const MyPostNotifyDummy = ({ notificationData }: any) => {
   const navigate = useNavigate();
-  interface Dummy {
-    title: string;
-    contents: string;
-  }
-  const DUMMY_CONTENT: Dummy = {
-    title: `<2023년 대한민국 고졸인재 채용엑스포 V-log 공모전 안내>`,
-    contents: `위 공모전에 관심있는 학생은 이진주 선생님에게 카카오워크로 연락주세요~!`,
-  };
   return (
     <S.MyPostNotifyWrap>
-      <S.MyPostNotify onClick={() => navigate("/read")}>
+      <S.MyPostNotify
+        onClick={() =>
+          navigate(`/write-read/${notificationData.notificationId}`)
+        }>
         <S.MyNotifyWrap>
           <S.MyNotifyTextWrap>
             <S.MyInfoWrap>
-              <S.MyProfile src={MyProfile} alt="내 프로필" />
+              <S.MyProfile
+                src={notificationData.profileImage}
+                alt="내 프로필"
+              />
               <S.MyInfoText>
-                <S.MyName>이진주T</S.MyName>
-                <S.MyNotifyDate>2023년 4월 20일(목) 02:56</S.MyNotifyDate>
+                <S.MyName>{notificationData.name}</S.MyName>
+                <S.MyNotifyDate>{notificationData.createdAt}</S.MyNotifyDate>
               </S.MyInfoText>
             </S.MyInfoWrap>
             <S.MyContentWrap>
               <S.MyContentTitleBox>
-                <S.MyContentTitle>{DUMMY_CONTENT.title}</S.MyContentTitle>
+                <S.MyContentTitle>{notificationData.title}</S.MyContentTitle>
                 <br />
               </S.MyContentTitleBox>
               <S.MyContentBox>
-                <S.MyContent>{DUMMY_CONTENT.contents}</S.MyContent>
+                <S.MyContent>{notificationData.contents}</S.MyContent>
               </S.MyContentBox>
             </S.MyContentWrap>
           </S.MyNotifyTextWrap>
           <S.MyNotifyImgWrap>
-            <S.MyNotifyImg src={MyNotifyImg} alt="공지 이미지" />
+            {notificationData.images && notificationData.images.length > 0 && (
+              <S.MyNotifyImg
+                src={notificationData.images[0].fileUrl}
+                alt="공지 이미지"
+              />
+            )}
           </S.MyNotifyImgWrap>
         </S.MyNotifyWrap>
         <S.MyNotifyLine></S.MyNotifyLine>
