db895cd7ca1e3daf8705030826b133c3fa789489
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-03-11 16:21:42 +0900

    feat :: Emoji 타입 지정 #94

--- src/constants/Emoji/Emoji.tsx
+++ src/constants/Emoji/Emoji.tsx
@@ -5,10 +5,16 @@ import CONFIG from "src/config/config.json";
 import * as S from "src/constants/Emoji/style/Emoji.style";
 
 const Emoji = () => {
+  interface EmojiData {
+    emojiName: string;
+    count: number;
+  }
   const accessToken =
     "eyJKV1QiOiJBQ0NFU1MiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiQXV0aG9yaXphdGlvbiI6IlRFQUNIRVIiLCJpYXQiOjE3MTAwNzkxODIsImV4cCI6MTcxMDA4MDk4Mn0.vOMTrCYqCY3j1DgOdw90etjv9XZYLbjtD_l4er-mQNGJSJeWQdZbJ7Y-FM05VcBCk6UzIC3GTyjS2u_sg6Wpqw";
   const { id } = useParams();
-  const [notificationEmojiData, setNotificationEmojiData] = useState<any>([]);
+  const [notificationEmojiData, setNotificationEmojiData] = useState<
+    EmojiData[]
+  >([]);
 
   useEffect(() => {
     const NotificationEmojiLoad = async () => {
@@ -42,7 +48,7 @@ const Emoji = () => {
   return (
     <S.MyNotificationEmojiWrap>
       <S.MyNotificationEmojiBox>
-        {notificationEmojiData.map((data: any) => (
+        {notificationEmojiData.map((data) => (
           <S.EmojiBox>
             <S.Emoji>{ChangeEmoji(data.emojiName)}</S.Emoji>
             <S.EmojiCnt>{data.count}</S.EmojiCnt>
