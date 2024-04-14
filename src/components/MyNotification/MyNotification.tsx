02273ae16265604da5aa9e62a9fcd21d983f9b9e
해준 <127281057+ftery0@users.noreply.github.com>
2024-04-14 21:02:17 +0900

    fix :: style 파일 오류

--- src/components/MyNotification/MyNotification.tsx
+++ src/components/MyNotification/MyNotification.tsx
@@ -1,6 +1,6 @@
+import * as S from "src/components/MyNotification/style";
 import SideBar from "src/components/SideBar/SideBar";
 import Mypost from "src/components/MyNotification/MyPostNotification";
-import * as S from "src/components/MyNotification/style/MyNotification.style";
 
 const MyNotification = () => {
   return (
