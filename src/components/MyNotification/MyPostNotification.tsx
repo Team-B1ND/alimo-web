d7ffc6c1a20b3bb32fc42bd43f5fb773eb8bb17a
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-04-24 12:33:02 +0900

    feat :: 공지글 보기 무한 스크롤 구현 #129

--- src/components/MyNotification/MyPostNotification.tsx
+++ src/components/MyNotification/MyPostNotification.tsx
@@ -10,7 +10,8 @@ import dayjs from "dayjs";
 import "dayjs/locale/ko";
 
 const MyPostNotification = () => {
-  const { notificationData, DataAbsence, DeleteButtonClick } = useMyNotification();
+  const { observerRef, notificationData, DataAbsence, DeleteButtonClick } =
+    useMyNotification();
   const navigate = useNavigate();
   const [, setIsImageError] = useState(true);
 
@@ -24,26 +25,38 @@ const MyPostNotification = () => {
         notificationData.map((notification: MyNotificationData) => (
           <S.MyPostNotificationWrap key={notification.notificationId}>
             <S.MyPostNotification>
-              <S.MyNotificationBox onClick={() => navigate(`/write-read/${notification.notificationId}`)}>
+              <S.MyNotificationBox
+                onClick={() =>
+                  navigate(`/write-read/${notification.notificationId}`)
+                }>
                 <S.MyNotificationTextWrap>
                   <S.MyInfoWrap>
-                    <S.MyProfile src={notification.profileImage || DefaultImg} onError={HandleImageError} />
+                    <S.MyProfile
+                      src={notification.profileImage || DefaultImg}
+                      onError={HandleImageError}
+                    />
                     <S.MyInfoText>
                       <S.MyName>{notification.name}</S.MyName>
                       <S.MyNotificationDate>
-                        {dayjs(notification.createdAt).locale("ko").format("YYYY년 MM월 DD일(ddd) HH:mm")}
+                        {dayjs(notification.createdAt)
+                          .locale("ko")
+                          .format("YYYY년 MM월 DD일(ddd) HH:mm")}
                       </S.MyNotificationDate>
                     </S.MyInfoText>
                   </S.MyInfoWrap>
                   <S.MyContentBoxWrap>
                     <S.MyContentTitleWrap>
-                      <S.MyContentTitle>{notification.title ? notification.title : ""}</S.MyContentTitle>
+                      <S.MyContentTitle>
+                        {notification.title ? notification.title : ""}
+                      </S.MyContentTitle>
                     </S.MyContentTitleWrap>
                     <S.MyContentWrap>
                       <S.MyContent>
-                        {notification.content ? notification.content.length < 50
-                          ? notification.content
-                          : `${notification.content.substring(0, 50)}...` : ""}
+                        {notification.content
+                          ? notification.content.length < 50
+                            ? notification.content
+                            : `${notification.content.substring(0, 50)}...`
+                          : ""}
                       </S.MyContent>
                     </S.MyContentWrap>
                   </S.MyContentBoxWrap>
@@ -64,8 +77,7 @@ const MyPostNotification = () => {
                   src={SettingImg}
                   onClick={() => {
                     DeleteButtonClick(notification);
-                  }}
-                ></S.DeleteButton>
+                  }}></S.DeleteButton>
               </S.DeleteButtonWrap>
             </S.MyPostNotification>
           </S.MyPostNotificationWrap>
@@ -73,6 +85,7 @@ const MyPostNotification = () => {
       ) : (
         <NullSkeleton />
       )}
+      {notificationData ? <S.ObserveBox ref={observerRef}></S.ObserveBox> : ""}
     </S.MyNotificationWrap>
   );
 };
--- src/hooks/Notification/useMyNotification.ts
+++ src/hooks/Notification/useMyNotification.ts
@@ -3,11 +3,24 @@ import { MyNotificationData } from "src/types/MyNotification/MyNotification.inte
 import { alimoV1Axios } from "src/libs/axios/CustomAxios";
 import Swal from "sweetalert2";
 import { showToast } from "src/libs/Toast/Swal";
+import { useInView } from "react-intersection-observer";
 
 const useMyNotification = () => {
-  const [notificationData, setNotificationData] = useState<MyNotificationData[]>([]);
+  const [pageNum, setPageNum] = useState(0);
+  const [notificationData, setNotificationData] = useState<
+    MyNotificationData[]
+  >([]);
   const [DataAbsence, setDataAbsence] = useState(true);
-  const [notificationDelete, setNotificationDelete] = useState<number>(0);
+  const [observerRef, inView] = useInView({
+    threshold: 0,
+    delay: 200,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      setPageNum((current) => current + 1);
+    }
+  }, [inView]);
 
   // 공지글 삭제
   const DeleteButtonClick = async (notification: MyNotificationData) => {
@@ -24,10 +37,12 @@ const useMyNotification = () => {
     }).then(async (result) => {
       if (result.isConfirmed) {
         try {
-          await alimoV1Axios.delete(`notification/delete/${notificationIdValue}`).then(() => {
-            showToast("success", "공지 삭제성공");
-            setNotificationDelete((prev) => prev + 1);
-          });
+          await alimoV1Axios
+            .delete(`notification/delete/${notificationIdValue}`)
+            .then(() => {
+              showToast("success", "공지 삭제성공");
+              setPageNum(1);
+            });
         } catch (error) {
           showToast("error", "공지 삭제실패");
           console.log(error);
@@ -38,28 +53,30 @@ const useMyNotification = () => {
 
   useEffect(() => {
     const MyNotificationLoad = async () => {
-      try {
-        await alimoV1Axios
-          .get(`notification/load/my`, {
-            params: { page: 1, size: 20 },
-          })
-          .then((res) => {
-            const NotificationData = res.data.data;
-            setNotificationData(NotificationData);
-            console.log(NotificationData);
-            if (NotificationData.length === 0) {
-              setDataAbsence(false);
-            }
-          });
-      } catch (error) {
-        console.log(error);
+      if (pageNum) {
+        try {
+          await alimoV1Axios
+            .get(`notification/load/my`, {
+              params: { page: pageNum, size: 20 },
+            })
+            .then((res) => {
+              const newNotificationData = res.data.data;
+              setNotificationData([...notificationData, ...newNotificationData]);
+              if (newNotificationData.length === 0) {
+                setDataAbsence(false);
+              }
+            });
+        } catch (error) {
+          console.log(error);
+        }
       }
     };
 
     MyNotificationLoad();
-  }, [notificationDelete]);
+  }, [pageNum]);
 
   return {
+    observerRef,
     notificationData,
     DataAbsence,
     DeleteButtonClick,
