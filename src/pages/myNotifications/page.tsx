7a8dc99277b9f00a523eeea564d5c2a9384c82f8
해준 <127281057+ftery0@users.noreply.github.com>
2024-05-12 19:31:38 +0900

    remove :: myNotification #134

--- src/pages/myNotifications/page.tsx
+++
@@ -1,7 +0,0 @@
-import MyNotification from "src/components/myNotifications/myNotifications";
-
-const MyNotificationPage = () => {
-  return <MyNotification />;
-};
-
-export default MyNotificationPage;
