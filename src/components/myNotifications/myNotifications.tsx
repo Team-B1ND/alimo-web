8a4221895a3ec859ab633c47cb4083acf098c6dd
해준 <127281057+ftery0@users.noreply.github.com>
2024-05-12 19:25:28 +0900

    fix :: 충돌 에러

--- src/components/myNotifications/myNotifications.tsx
+++ src/components/myNotifications/myNotifications.tsx
@@ -1,12 +1,10 @@
 import * as S from "src/components/myNotifications/style";
-import SideBar from "src/components/sideBar/sideBar";
 import Mypost from "src/components/myNotifications/myPostNotifications";
 
 
 const MyNotification = () => {
   return (
     <S.MyNotification>
-      <SideBar />
       <S.MyNotificationView>
         <Mypost />
       </S.MyNotificationView>
