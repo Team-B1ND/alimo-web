215f1521622acc041ff0f3c028c36a62a5cb493b
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-05-13 18:08:08 +0900

    design :: Skeleton-UI 변경 & ErrorSkeleton 생성 #132

--- src/components/myNotifications/skeleton/nullSkeleton.tsx
+++ src/components/myNotifications/skeleton/nullSkeleton.tsx
@@ -3,7 +3,7 @@ import * as S from "src/components/myNotifications/style";
 const NullSkeleton = () => {
   return (
     <S.MyPostNotificationWrap>
-      <S.NullSkelton>작성한 공지글이 없습니다</S.NullSkelton>
+      <S.Skeleton>작성한 공지글이 없습니다</S.Skeleton>
     </S.MyPostNotificationWrap>
   );
 };
--- src/components/myNotifications/skeleton/skeleton.tsx
+++ src/components/myNotifications/skeleton/skeleton.tsx
@@ -7,9 +7,22 @@ interface Props {
 const Skeleton = ({ key }: Props) => {
   return (
     <S.MyPostNotificationWrap key={key}>
-      <S.SkeltonWrap>
-        <S.Skelton></S.Skelton>
-      </S.SkeltonWrap>
+      <S.SkeletonWrap>
+        <S.SkeletonBox>
+          <S.MyNotificationTextWrap>
+            <S.MyInfoWrap>
+              <S.ProfileSkeleton></S.ProfileSkeleton>
+              <S.MyInfoText>
+                <S.MyInfoSkeleton></S.MyInfoSkeleton>
+              </S.MyInfoText>
+            </S.MyInfoWrap>
+            <S.MyContentBoxWrap>
+              <S.TitleSkeleton></S.TitleSkeleton>
+              <S.ContentSkeleton></S.ContentSkeleton>
+            </S.MyContentBoxWrap>
+          </S.MyNotificationTextWrap>
+        </S.SkeletonBox>
+      </S.SkeletonWrap>
     </S.MyPostNotificationWrap>
   );
 };
