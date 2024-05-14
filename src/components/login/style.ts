ef86ef79c63da9a752b37033bc986e9c8af6e50c
해준 <127281057+ftery0@users.noreply.github.com>
2024-05-15 00:53:40 +0900

    feat :: disabled추가

--- src/components/login/style.ts
+++ src/components/login/style.ts
@@ -45,6 +45,9 @@ export const LoginWrap = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+  input:disabled{
+    background: #fff;
+  }
 `;
 
 export const LogoWrap = styled.div`
