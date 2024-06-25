bea5de079458dd7e166aa178643ab64199ebf8d1
해준 <127281057+ftery0@users.noreply.github.com>
2024-06-25 19:33:05 +0900

    chore :: 짜잘한 코드 삭제

--- src/components/settingPage/setting.tsx
+++ src/components/settingPage/setting.tsx
@@ -2,28 +2,47 @@ import CloseButton from "src/assets/images/common/Closeimg.png";
 import * as S from "./style";
 
 const Setting = ({ onClose }: { onClose: () => void }) => {
-
-  const handClose = ()=>{
-
-  }
-
   return (
-    <S.Setting  onClick={()=>onClose()}>
+    <S.Setting onClick={() => onClose()}>
       <S.SettingMain>
-        <S.SettingTitle onClick={()=>onClose()}>
+        <S.SettingTitle onClick={() => onClose()}>
           <span>설정</span>
         </S.SettingTitle>
         <img src={CloseButton} />
-      
-        <S.Settingexplanation onClick={()=>onClose()} >
+
+        <S.Settingexplanation onClick={() => onClose()}>
           <S.SettingDetail>
-            <span onClick={()=>window.open("https://alimo.b1nd.com/detailed-information/service-policy")}>서비스 정책</span>
+            <span
+              onClick={() =>
+                window.open(
+                  "https://alimo.b1nd.com/detailed-information/service-policy"
+                )
+              }
+            >
+              서비스 정책
+            </span>
           </S.SettingDetail>
           <S.SettingDetail>
-            <span onClick={()=>window.open("https://alimo.b1nd.com/detailed-information/personal-information")}>개인정보 이용 약관</span>
+            <span
+              onClick={() =>
+                window.open(
+                  "https://alimo.b1nd.com/detailed-information/personal-information"
+                )
+              }
+            >
+              개인정보 이용 약관
+            </span>
           </S.SettingDetail>
           <S.SettingDetail>
-            <span onClick={()=>window.open("https://alimo.b1nd.com/detailed-information/usage-guide")}>사용방법</span>
+            <span
+              onClick={() =>
+                window.open(
+                  "https://alimo.b1nd.com/detailed-information/usage-guide"
+                )
+              }
+            >
+              사용방법
+            </span>
           </S.SettingDetail>
         </S.Settingexplanation>
       </S.SettingMain>
